af8dc242483e52a4543f37dba8c18b9ff6067822
Marco Barão <marco.barao@outlook.com>
2018-10-25 22:40:35 -0300

    CORS e finalizando

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require("cors");
 
 const config = require("./config");
 
@@ -16,8 +17,9 @@ const customerRoute = require("./routes/customer.route");
 const orderRoute = require("./routes/order.route");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/", indexRoute);
 app.use("/products", productRoute);
